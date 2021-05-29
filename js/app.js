134c31cca645096b413db783a0d81db2baa730e3
Яна Белых <61792507+yanabelykh18@users.noreply.github.com>
2021-05-29 21:25:37 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#000000",
         "opacity": 0.4,
         "width": 1
       },
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#00ff00",
+      "background_color": "#ffff00",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
