82018544bc6c15eb1545d34597aad6dc0b16e8fa
Яна Белых <61792507+yanabelykh18@users.noreply.github.com>
2021-05-29 21:16:37 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#00CED1",
+      "background_color": "#9370DB",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
